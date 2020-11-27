be18c1a82cded67739b386982efa163fcfca51de
WilsMcCreight <wilsmccreight@icloud.com>
2020-11-27 00:26:16 -0500

    Finished bot logging/db insertion

--- bot.py
+++ bot.py
@@ -1,50 +1,68 @@
-# bot.py
-import os
-import random
+# # bot.py
+# import os
+# import random
 
-from discord.ext import commands
-from dotenv import load_dotenv
-import pandas as pd
-import sqlite3
+# from discord.ext import commands
+# from dotenv import load_dotenv
+# import pandas as pd
+# import sqlite3
 
-from db_control import connect_db
-
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-bot = commands.Bot(command_prefix='!')
-
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-
-@bot.command(name='quote', help='Saves a quote to the quote book')
-async def save_quote(ctx):
-    
+# from db_control import connect_db, insert_quote
 
 
-    await ctx.send('Ayo')
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
 
-@bot.command(name='roll', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
+# bot = commands.Bot(command_prefix='!')
 
-bot.run(TOKEN)
+# # bot = commands.Bot(command_prefix=when_mentioned)
+
+# @bot.command(when_mentioned)
+# async def test(ctx, arg):
+#     if arg == "ppp":
+#         print("IT WORKED")
+#     print("IT MIGHT HAVE WORKED")
+
+
+
+# @bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+# async def nine_nine(ctx):
+#     brooklyn_99_quotes = [
+#         'I\'m the human form of the 💯 emoji.',
+#         'Bingpot!',
+#         (
+#             'Cool. Cool cool cool cool cool cool cool, '
+#             'no doubt no doubt no doubt no doubt.'
+#         ),
+#     ]
+
+#     response = random.choice(brooklyn_99_quotes)
+#     await ctx.send(response)
+
+# @bot.command(name='q', help='Saves a quote to the quote book')
+# async def save_quote(ctx, *, arg):
+#     print(arg)
+#     await ctx.send('Ayo')
+
+# @bot.command(name='quote', help='Saves a quote to the quote book')
+# async def save_quote_(ctx):
+#     await ctx.send('Ayo')
+
+# @bot.command()
+# async def save_quote__(ctx, arg):
+#     if arg == "ppp":
+#         print("IT WORKED")
+#     pass
+
+# @bot.command(name='roll', help='Simulates rolling dice.')
+# async def roll(ctx, number_of_dice: int, number_of_sides: int):
+#     dice = [
+#         str(random.choice(range(1, number_of_sides + 1)))
+#         for _ in range(number_of_dice)
+#     ]
+#     await ctx.send(', '.join(dice))
+
+# bot.run(TOKEN)
 
 
 
@@ -84,47 +102,100 @@ bot.run(TOKEN)
 
 
 
-# # bot.py
-# import os
-# import random
+# bot.py
+import os
+import discord
+from dotenv import load_dotenv
 
-# import discord
-# from dotenv import load_dotenv
+from db_control import connect_db, insert_quote, insert_quote_small
 
-# load_dotenv()
-# TOKEN = os.getenv('DISCORD_TOKEN')
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+BOT_ID = os.getenv('BOT_MENTION_ID')
 
-# client = discord.Client()
+client = discord.Client()
 
-# @client.event
-# async def on_ready():
-#     print(f'{client.user.name} has connected to Discord!')
-
-# # @client.event
-# # async def on_member_join(member):
-# #     await member.create_dm()
-# #     await member.dm_channel.send(
-# #         f'Hi {member.name}, welcome to my Discord server!'
-# #     )
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
 
 # @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#         f'Hi {member.name}, welcome to my Discord server!'
+#     )
 
-#     brooklyn_99_quotes = [
-#         'I\'m the human form of the 💯 emoji.',
-#         'Bingpot!',
-#         (
-#             'Cool. Cool cool cool cool cool cool cool, '
-#             'no doubt no doubt no doubt no doubt.'
-#         ),
-#     ]
+@client.event
+async def on_message(message):
+    print(message.author)
+    if message.author == client.user:
+        return
+    if(message.content.startswith(BOT_ID)):
+        print("Quote Bot was mentioned")
+        text = message.content[len(BOT_ID):].strip()
+        if text.startswith('help'):
+            await message.channel.send("""Hi, I'm Quote Bot! You can use me to log quotes into a database from which they can be retrieved later. The general quote logging format is as follows:
+            1. Quote Bot mention
+            2. Quote (Surrounded by quotation marks ("))
+            3. Hyphen separator (Optional)
+            4. Quotee name
+            5. Context
 
-#     if message.content == '99!':
-#         response = random.choice(brooklyn_99_quotes)
-#         await message.channel.send(response)
-#     elif message.content == 'raise-exception':
-#         raise discord.DiscordException
+            An example would be:    @Quote Bot "Eatin ass and eatin ass" - Triggs referring to a canibal orgy
+            Or without the hyphen:  @Quote Bot “My therapist told me not to kink shame so I won’t” SJ in reference to D&D citizens being attracted to brogmir and a lot of other stuff
+            
+            You can also log a quick quote by simply mentioning Quote Bot and following that with a quote. 
 
-# client.run(TOKEN)
+            For example:    @Quote Bot Brogmir have arm, Brogmir is happy
+            Or:             @Quote Bot "Brogmir lost arm, Brogmir is sad"
+
+            These examples will forego the metadata of the quotee's name and the quote's context.
+
+            Ways to retreive quotes and visualize them are underway, stay tuned for more functionality and feel free to send any ideas or feedback to Wils!
+            """)
+
+        quote_indices = [i for i, ltr in enumerate(text) if ltr == '"']
+
+        if len(quote_indices) == 0:
+            text = '"' + text + '"'
+            print("Inserting a small quote:", text)
+            if message.author.nick:
+                log_quote((message.author.nick, text), insert_quote_small)
+            else:
+                log_quote((message.author.name, text), insert_quote_small)
+
+        elif len(quote_indices)%2 != 0:
+            await message.channel.send('Sorry, the number of quotation marks (") was odd. Not sure how to parse that quote.')
+
+        else:
+            print("Inserting a big quote:", text)
+            if message.author.nick:
+                author_name = message.author.nick
+            else:
+                author_name = message.author.name
+            quote_text = text[quote_indices[0]:quote_indices[-1]+1]
+            meta_data = text[quote_indices[-1]+1:]
+            meta_data = meta_data.strip()
+            if meta_data[0] == '-':
+                meta_data = meta_data[1:].strip()
+            meta_data = meta_data.split()
+            print("SPlit meta_data:", meta_data)
+            quotee_name = meta_data[0]
+            context = (" ").join(meta_data)
+            # print('quote_text:', quote_text)
+            # print('meta_data:', meta_data)
+            log_quote((author_name, quotee_name, quote_text, context), insert_quote)
+
+
+
+def log_quote(quote, insert_func):
+    conn = connect_db('Quote.db')
+    if (not conn):
+        print('Bot failed to connect to database, exiting')
+    else:
+        curs = conn.cursor()
+        insert_func(conn, curs, quote)
+        conn.close()    
+
+client.run(TOKEN)
--- db_control.py
+++ db_control.py
@@ -13,10 +13,11 @@ def run_commit(sql,conn,curs):
 def creat_tQuote(conn, curs):
     sql = """ CREATE TABLE IF NOT EXISTS tQuote (
                 id integer PRIMARY KEY,
-                logger_name text,
-                quoter_name text,
+                logger_name text NOT NULL,
+                quotee_name text,
                 quote text NOT NULL,
-                date text
+                context text,
+                date DATETIME DEFAULT CURRENT_TIMESTAMP
             ); """
     run_commit(sql, conn, curs)
 
@@ -37,10 +38,23 @@ def backup_log(quote):
         f.write(str(quote) + '\n')
 
 def insert_quote(conn, curs, quote):
-    sql = ''' INSERT INTO tQuote(logger_name,quoter_name,quote,date)
+    sql = ''' INSERT INTO tQuote(logger_name,quotee_name,quote,context)
               VALUES(?,?,?,?)'''
     try:
         curs.execute(sql, quote)
+        # curs.execute('INSERT INTO tQuote (LastModifiedTime) VALUES(CURRENT_TIMESTAMP)')
+        conn.commit()
+    except Error as e:
+        print(e)
+    backup_log(quote)
+    return curs.lastrowid
+
+def insert_quote_small(conn, curs, quote):
+    sql = ''' INSERT INTO tQuote(logger_name,quote)
+              VALUES(?,?)'''
+    try:
+        curs.execute(sql, quote)
+        # curs.execute('INSERT INTO tQuote (LastModifiedTime) VALUES(CURRENT_TIMESTAMP)')
         conn.commit()
     except Error as e:
         print(e)
@@ -56,29 +70,30 @@ if __name__ == "__main__":
     elif (sys.argv[1] == '-help') or (sys.argv[1] == '-h'):
         print("""Command List:
         -h, -help      :  Review command options
-        -rb, -rebuild  :  DELTES and rebuilds the QuotesDB database
+        -rb, -rebuild  :  DELTES and rebuilds the QuoteDB database
         -v, -view      :  Prints all entries in the database
         """)
     else:
-        db_name = 'Quotes.db'
+        db_name = 'Quote.db'
         if (sys.argv[1] == '-rb') or (sys.argv[1] == '-rebuild'):
             inp = input("Are you sure you would like to delete the existing database? (Y/N): ")
             if inp.lower() == 'n':
                 print('Phew, close call')
                 sys.exit()
             print("Rebuilding database")
-            os.remove(db_name)
+            if os.path.exists(db_name):
+                os.remove(db_name)
             conn = connect_db(db_name)
             if (not conn):
-                print('Failed to build database, exiting')
+                print('Failed to rebuild database, exiting')
             else:
                 curs = conn.cursor()
                 build_tables(conn, curs)
-                print("Successfully rebuilt Quotes.db database")
+                print("Successfully rebuilt Quote.db database")
         elif (sys.argv[1] == '-v') or (sys.argv[1] == '-view'):
             conn = connect_db(db_name)
             if (not conn):
-                print('Failed to build database, exiting')
+                print('Failed to connect to database, exiting')
             else:
                 curs = conn.cursor()
                 # print(pd.read_sql("SELECT * FROM sqlite_master;",conn))
@@ -86,10 +101,10 @@ if __name__ == "__main__":
         elif(sys.argv[1] == '-t') or (sys.argv[1] == '-test'):
             conn = connect_db(db_name)
             if (not conn):
-                print('Failed to build database, exiting')
+                print('Failed to connect to database, exiting')
             else:
                 curs = conn.cursor()
-                quote = ('Wils', 'Lee', 'Eatin ass and eatin ass', '2020-11-22')
+                quote = ('Wils', 'Lee', 'Eatin ass and eatin ass', 'Triggs referring to a canibal orgy')
                 print(insert_quote(conn, curs, quote))
         conn.close()
     
